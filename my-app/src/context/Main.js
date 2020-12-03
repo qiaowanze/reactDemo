aad6a734ccf82b8a33b764e517bd041c05003e77
qiaodong <10862137@qq.com>
2020-12-03 14:22:09 +0800

    feat:redux原理探索

--- my-app/src/context/Main.js
+++ my-app/src/context/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Main extends Component {
+    // 在这里 自组件想要使用context 就必须使用contextTypes
     static contextTypes = {
         themeColor: PropTypes.string
     }
@@ -11,7 +12,7 @@ export default class Main extends Component {
     render() {
         return (
             <div>
-                <h1 style={{ color: this.context.themeColor }}>这里是头部标题</h1>
+                <h1 style={{ color: this.context.themeColor }}>这里是主要内容</h1>
             </div>
         )
     }
